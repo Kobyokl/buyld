f764ae493d7d0ea322ddb7b90cf877b64514caf7
Kobe Youmans <youmanskobe@gmail.com>
2026-08-01 01:00:17 -0400

    im tired

--- src/main.c
+++ src/main.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 #include <stdint.h> 
-#include <string.h>
+#include <math.h>
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h> 
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+#define PI 3.1415
+#define HFOV (0.77f * SCREEN_HEIGHT)
+#define VFOV (0.2f * SCREEN_HEIGHT)
+
 
 typedef struct vec2d { int x; int y; } vec2d_t; 
 typedef struct vec3d { int x; int y; int z; } vec3d_t; 
-
 typedef struct fvec2d { float x; float y; } fvec2d_t; 
 typedef struct fvec3d { float x; float y; float z; } fvec3d_t; 
 
 
 typedef struct state { // abstract state that can apply to multipe objects
     int last_sector; //this is lastknownsector from the build style documnets thingy....
-    int id; // id of whatever entity.... player is 0, anyting else is tbd
+    unsigned char id; // id of whatever entity.... player is p, anyting else is tbd... made it unsigned jus cuz idk maybe there'll be a purpose for this 
 } state_t; 
 
 typedef struct engine { 
@@ -47,63 +50,30 @@ typedef struct level {
 } level_t; 
 
 typedef struct player {
-    vec2d_t position
-    vec2d_t look_angle
-    int curr_sector
+    fvec2d_t position;
+    state_t state; // has the player's id as well as the player's last position
+    float look_angle; // in radians
 } player_t; 
 
 int s2i(const char *str); 
+void parse_level_data(FILE *level_data, level_t *level, int sector_count);
 
-void parse_level_data(FILE *level_data, level_t *level, int sector_count){j
+vec2d_t intersection(x1,y1, x2,y2, x3,y3, x4,y4){ 
+    /* 
+        returns the point of intersection of two lines (x1, y1) -> (x2, y2) AND (x3, y3) -> (x4, y4) 
+        
+        if they dont intersect then we will return a vector w/ NAN so yeaaa
+    */
+    float denom = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4)); 
+    if (denom == 0.0) return (fvec2d_t) = { .x = NAN, .y = NAN};  
+
+    fvec2d_t retvec; 
     
-    if (level_data == NULL) {
-        printf("Level data isn't found.");
-        return; 
-    }
+    retvec.x = (((x1 * y2 - y1 * x2) * (x3 - x4)) - ((x1 - x2) * (x3 * y4 - y3 * x4))) / denom; 
+    retvec.y = (((x1 * y2 - y1 * x2) * (y3 - y4)) - ((y1 - y2) * (x3 * y4 - y3 * x4))) / denom; 
+    
+    return retvec; 
 
-    sector_t level_sectors[sector_count];
-    sector_t level_sector;
-
-    char buffer[100];
-
-    while(buffer[0] != '$'){ fgets(buffer, sizeof(buffer), level_data); } // get to the sectors in the level data
-    fgets(buffer, sizeof(buffer), level_data);
-
-    printf("%s", buffer); 
-
-
-    level_sector.id = (int) buffer[0] - '0'; 
-    level_sector.num_walls = (int) buffer[2] - '0'; 
-    level_sector.wall_index = (int) buffer[4] - '0'; 
-    level_sector.ceiling_height = (float) buffer[6] - '0'; 
-    level_sector.floor_height = (float) buffer[8] - '0'; 
-
-    sector_count[0] = level_sector; 
-
-    while(buffer[0] != '$'){ fgets(buffer, sizeof(buffer), level_data); } // get to the walls in the level data
-    fgets(buffer, sizeof(buffer), level_data);
-
-    wall_t walls[level_sector.num_walls]; // 5 == level_sector.num_walls
-
-    for(size_t i = 0; i < level_sector.num_walls; i++){
-        walls[i] = (wall_t) {
-            .index = (int) buffer[0] - '0', 
-            .start_pos = {
-                .x = (int) buffer[2] - '0',
-                .y = (int) buffer[4] - '0',
-            },
-            .end_pos = {
-                .x = (int) buffer[6] - '0',
-                .y = (int) buffer[8] - '0',
-            },
-            .portal = s2i(buffer + 10),
-        };
-        fgets(buffer, sizeof(buffer), level_data);
-    }
-
-
-    level->sectors = level_sectors;
-    level->walls = walls;
 }
 
 
@@ -113,29 +83,61 @@ int main(int argc, char *argv[]){
 
     level_t LEVEL; 
 
-    parse_level(level_data, &LEVEL, 3);
-    
-    
-    printf("%d\n", level_sector.id);
-    printf("%d\n", level_sector.num_walls);
-    printf("%d\n", level_sector.wall_index);
-    printf("%f\n", level_sector.ceiling_height);
-    printf("%f\n", level_sector.floor_height);
+    parse_level_data(level_data, &LEVEL, 3);
+
+    player_t player = {
+        .position = (fvec2d_t) {
+            .x = 3.0, 
+            .y = 2.0,
+        },
+        .look_angle = PI/2,
+        .state = (state_t) {
+            .id = 'p',
+            .last_sector = 0, 
+        }
+
+    };
+
+    // just code for LEVEL.sectors[0]
+    for(int i = 0; i < LEVEL.sectors[0].num_walls; i++){
+        
+        // change to player basis (think change of basis in linear algebra..) 
+        fvec2d_t rv1 = { .x = LEVEL.walls[i].start_pos.x - player.position.x, .y = LEVEL.walls[i].start_pos.y - player.position.y };
+        fvec2d_t rv2 = { .x = LEVEL.walls[i].end_pos.x - player.position.x, .y = LEVEL.walls[i].end_pos.y - player.position.y }; 
+        // ^^^^ these are the positions of teh walls relatiive to teh player.. now we have to rotate them using js the 2d rotation matrix
+        int tempx = rv1.x; 
+        rv1.x = (rv1.x * sinf(player.look_angle) - rv1.y * cosf(player.look_angle));
+        rv1.y = (tempx * sinf(player.look_angle) + rv1.y * cosf(player.look_angle));
+        
+        tempx = rv2.x;
+        rv2.x = (rv2.x * sinf(player.look_angle) - rv2.y * cosf(player.look_angle));
+        rv2.y = (tempx * sinf(player.look_angle) + rv2.y * cosf(player.look_angle));
+
+        if (rv1.y <= 0 && rv2.y <= 0) continue; // the wall is cmpletely behind the camera then we dont have to render it
+
+        if (rv1.y <= 0 || rv2.y <= 0){ // check if part of the wall is behind the player and if it is then we need to do clipping trick so our shi isnt fucked up
+            float neary = 1e-4;
+            float d1 = rv1.y - neary; 
+            float d2 = rv2.y - neary; 
+
+            float t = d1 / (d1 - d2); 
+            float ix = rv1.x + t * (rv2.x - rv1.x);
+            float iy = rv1.y + t * (rv2.y - rv1.y);
+        }
 
 
+        // here's perspective transformation.. the first two variabels are how much we scale the x and y coordinates to map them onto screen space
+        float xscale1 = HFOV / rv1.y, yscale1 = VFOV / rv1.y; 
+        float xscale2 = HFOV / rv2.y, yscale2 = VFOV / rv2.y;  
 
-    printf("%d\n", walls[0].index);
-    printf("%d %d\n", walls[0].start_pos.x, walls[0].start_pos.y);
-    printf("%d %d\n", walls[0].end_pos.x, walls[0].end_pos.y);
-    printf("%d\n", walls[0].portal);
+        int x1 = W/2 - (int)(rv1.x * xscale1); 
+        int x2 = W/2 - (int)(rv2.x * xscale2); 
 
-    printf("%d\n", walls[1].index);
-    printf("%d %d\n", walls[1].start_pos.x, walls[1].start_pos.y);
-    printf("%d %d\n", walls[1].end_pos.x, walls[1].end_pos.y);
-    printf("%d\n", walls[1].portal);
+        if (x1 >= x2 || x2 < 0 || x1 > SCREEN_WIDTH) continue ;
 
-    
-    return 0; 
+
+    }
+
 }
 
 int s2i(const char *str) {
@@ -164,4 +166,56 @@ break;
 return sign * result;
 }
 
+void parse_level_data(FILE *level_data, level_t *level, int sector_count){
+    
+    if (level_data == NULL) {
+        printf("Level data isn't found.");
+        return; 
+    }
+
+    char buffer[100];
+
+    
+    level->sectors = malloc(sizeof(sector_t) * sector_count); 
+
+    { // traverses the buffer to get to meaningful data
+        while(fgets(buffer, sizeof(buffer), level_data)){if(buffer[0] == '$') break;} // get to the sectors in the level data
+        fgets(buffer, sizeof(buffer), level_data);
+    }
+
+    sscanf(buffer, 
+        "%d %d %d %f %f",
+        &level->sectors[0].id, 
+        &level->sectors[0].num_walls,
+        &level->sectors[0].wall_index,
+        &level->sectors[0].ceiling_height,
+        &level->sectors[0].floor_height
+    ); 
+    
+    level->walls = malloc(sizeof(wall_t) * level->sectors[0].num_walls); 
+
+    { // traverses the buffer to get to meaningful data
+        while(fgets(buffer, sizeof(buffer), level_data)){if(buffer[0] == '$') break;}  // get to the walls in the level data
+        fgets(buffer, sizeof(buffer), level_data);
+    }
+
+
+    for(size_t i = 0; i < level->sectors[0].num_walls; i++){
+        sscanf(
+            buffer,
+            "%d %d %d %d %d %d", 
+            &level->walls[i].index, 
+            &level->walls[i].start_pos.x, 
+            &level->walls[i].start_pos.y, 
+            &level->walls[i].end_pos.x, 
+            &level->walls[i].end_pos.y, 
+            &level->walls[i].portal
+        );
+        fgets(buffer, sizeof(buffer), level_data);
+    }
+
+    // the reason this is returning garbage value is because level_sectors and walls are addresses that have memory allocated to them on the stack... however once this 
+    // function returns and the stack pointer increments back to normal, level_sectors and walls point to garbage values... sooo we need to
+    // allocate them on the heap with malloc. 
+}
 
